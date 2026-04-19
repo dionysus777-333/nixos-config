c3b5ed241bbf3eae9ee99ebbf3253840c185a0d9
dionysus777-333 <eric.z.dino@outlook.com>
2026-04-18 17:44:23 -0700

    anki test

--- configuration.nix
+++ configuration.nix
@@ -38,6 +38,7 @@ let
     yazi
     zathura
     zotero
+    localsend
   ]);
   extra = minimal ++ (with pkgs; 
   [
--- modules/anki.nix
+++ modules/anki.nix
@@ -6,6 +6,18 @@
     (pkgs.anki.withAddons [
       pkgs.ankiAddons.anki-connect
       pkgs.ankiAddons.review-heatmap
+      (pkgs.anki-utils.buildAnkiAddon (finalAttrs: {
+        pname = "AJT Japanese";
+        version = "v25.5.15.0";
+        src = pkgs.fetchFromGitHub {
+          owner = "Ajatt-Tools";
+          repo = "Japanese";
+          rev = "1c993fdb90c771fd5e154bd4e2b57e12a3ca1991";
+          hash = "sha256-oQ38Gmvwp1dSgrUZA9pz4DAxPrbQDrhsGoi+BSNh3fs=";
+          fetchSubmodules = true;
+        };
+        sourceRoot = "${finalAttrs.src.name}/japanese";
+      }))
     ])
   ];
 }
